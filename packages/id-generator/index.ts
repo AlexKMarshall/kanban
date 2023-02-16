f66c6c96c236738e3364165e22265ec8d7672afe
Alex Marshall <alexmarshall@qogita.com>
2023-02-16 07:52:39 +0000

    feat: create id-generator package (#6)

---
+++ packages/id-generator/index.ts
@@ -0,0 +1,18 @@
+import { v4, v5 } from 'uuid'
+
+export interface IdGenerator {
+  next(): string
+}
+
+export class UuidV4Generator {
+  n: number = 1
+  next = v4
+}
+
+export class UuidV5Generator {
+  private n: number = 1
+
+  constructor(private namespace: string) {}
+
+  next = () => v5((this.n++).toString(), this.namespace)
+}
