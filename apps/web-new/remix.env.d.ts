8e7c9363387aabc7a18c0d1f641201560d0dd0ff
AlexKMarshall <alexmarshall@qogita.com>
2023-02-21 08:10:36 +0000

    test: add in middleware to test app

--- apps/web-new/remix.env.d.ts
+++ apps/web-new/remix.env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+import type { Context } from './app/context'
+
+declare module '@remix-run/node' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface AppLoadContext extends Context {}
+}
