f003bbed68bf629f748974128e581d40a7a36662
AlexKMarshall <alexmarshall@qogita.com>
2023-02-21 08:29:50 +0000

    fix: add esbuild dependency to make server script work

--- apps/web-new/app/routes/boards.tsx
+++ apps/web-new/app/routes/boards.tsx
@@ -1,11 +1,11 @@
+import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 
-export function loader() {
-  return json([
-    { name: 'Project 1', id: 1 },
-    { name: 'Project new', id: 2 },
-  ])
+export async function loader({ context }: LoaderArgs) {
+  const boards = await context.db.board.findMany()
+
+  return json({ boards, count: boards.length })
 }
 
 export default function Boards() {
