21b1e05f49019a42a093726d1a978bd4535f1821
AlexKMarshall <alexmarshall@qogita.com>
2023-02-21 08:10:36 +0000

    test: add in middleware to test app

--- apps/web-new/test/TestApp.tsx
+++ apps/web-new/test/TestApp.tsx
@@ -1,40 +1,91 @@
 import { unstable_createRemixStub } from '@remix-run/testing'
 import type { ServerRouteModule } from '@remix-run/server-runtime/dist/routeModules'
+import { sleep } from '@kanban/clock'
+import {
+  type ActionArgs,
+  type ActionFunction,
+  type LoaderArgs,
+  type LoaderFunction,
+} from '@remix-run/node'
+import { createPrismaMock } from '@kanban/database/mock'
 
 import * as IndexModule from '../app/routes'
 import * as BoardsModule from '../app/routes/boards'
+import { type TestContext, createTestContext } from './test-context'
+import { json } from '@remix-run/server-runtime'
+
+type DataFunction = LoaderFunction | ActionFunction
+type DataArgs = LoaderArgs | ActionArgs
+type Middleware = (
+  fn: DataFunction
+) => (args: DataArgs) => ReturnType<DataFunction>
 
 type TestAppStoryProps = {
   url: TestAppProps['url']
 }
 
 export function TestAppStory({ url }: TestAppStoryProps) {
-  return <TestApp url={url} />
+  const context = createTestContext({
+    db: createPrismaMock(),
+  })
+  return <TestApp url={url} context={context} />
 }
 
 type TestAppProps = {
   url: string
+  context: TestContext
+  delay?: number
 }
-function TestApp({ url }: TestAppProps) {
+function TestApp({ url, context, delay = 0 }: TestAppProps) {
+  const middleware: Middleware = (fn) => async (args) => {
+    await sleep(0)
+    let response
+    try {
+      response = await fn({ ...args, context })
+    } catch (error) {
+      if (error instanceof Response) {
+        response = error
+      } else if (error instanceof Error) {
+        response = json(
+          { message: error.message, stack: error.stack },
+          { status: 500 }
+        )
+      } else {
+        response = json(
+          { message: typeof error === 'string' ? error : 'Unknown error' },
+          { status: 500 }
+        )
+      }
+      throw response
+    }
+    await sleep(delay)
+    return response
+  }
+
+  const RemixStub = unstable_createRemixStub([
+    { path: '/', ...routeFromModule({ module: IndexModule, middleware }) },
+    {
+      path: '/boards',
+      ...routeFromModule({ module: BoardsModule, middleware }),
+    },
+  ])
+
   return <RemixStub initialEntries={[url]} />
 }
-
-const RemixStub = unstable_createRemixStub([
-  { path: '/', ...routeFromModule({ module: IndexModule }) },
-  { path: '/boards', ...routeFromModule({ module: BoardsModule }) },
-])
 
 type Route = Parameters<typeof unstable_createRemixStub>[0][number]
 function routeFromModule({
   module,
+  middleware,
 }: {
   module: Partial<ServerRouteModule>
+  middleware: Middleware
 }): Route {
   const Component = module.default
 
   return {
     // @ts-expect-error React Router types have context as optional, but we're making them required
-    loader: module.loader ? module.loader : undefined,
+    loader: module.loader ? middleware(module.loader) : undefined,
     element: Component ? <Component /> : undefined,
   }
 }
