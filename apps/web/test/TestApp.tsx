aa267ee94ae68182b17678a5aa257b802fd2d20c
Alex Marshall <alexmarshall@qogita.com>
2023-03-02 18:03:27 +0000

    Alexkmarshall83/ale 52 simulate network delay in storybook (#50)
    * feat: add network delay option to storybook
    
    * test: randomize network delay

--- apps/web/test/TestApp.tsx
+++ apps/web/test/TestApp.tsx
@@ -28,11 +28,24 @@ type TestAppStoryProps = {
   boards: Array<Parameters<typeof buildBoard>[0]>
   columns: Array<Parameters<typeof buildColumn>[0]>
   tasks: Array<Parameters<typeof buildTask>[0]>
+  connection: 'instant' | 'very fast' | 'fast' | 'slow' | 'very slow'
 }
 
 export const testAppStoryDefaultProps = {
   url: '/',
   ...fullBoardData,
+  connection: 'very fast',
+} satisfies Partial<TestAppStoryProps>
+
+const randomBetween = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min)
+
+const networkDelayMap: Record<TestAppStoryProps['connection'], () => number> = {
+  instant: () => 0,
+  'very fast': () => randomBetween(0, 100),
+  fast: () => randomBetween(100, 500),
+  slow: () => randomBetween(500, 1000),
+  'very slow': () => randomBetween(1000, 2000),
 }
 
 export function TestAppStory({
@@ -40,19 +53,22 @@ export function TestAppStory({
   boards,
   columns,
   tasks,
+  connection,
 }: TestAppStoryProps) {
   const context = createTestContext({
     db: createPrismaMock({ data: createSeedData({ boards, columns, tasks }) }),
   })
-  return <TestApp url={url} context={context} />
+  return (
+    <TestApp url={url} context={context} delay={networkDelayMap[connection]} />
+  )
 }
 
 type TestAppProps = {
   url: string
   context: TestContext
-  delay?: number
+  delay?: () => number
 }
-function TestApp({ url, context, delay = 0 }: TestAppProps) {
+function TestApp({ url, context, delay = () => 0 }: TestAppProps) {
   const middleware: Middleware = (fn) => async (args) => {
     await sleep(0)
     let response
@@ -74,7 +90,7 @@ function TestApp({ url, context, delay = 0 }: TestAppProps) {
       }
       throw response
     }
-    await sleep(delay)
+    await sleep(delay())
     return response
   }
 
