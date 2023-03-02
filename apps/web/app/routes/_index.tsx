bb7dff44178d565279c46c4a35d954711b9e8f4d
AlexKMarshall <alexmarshall@qogita.com>
2023-03-02 18:22:14 +0000

    feat: install vanilla extract

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,9 +1,10 @@
 import { Link } from '@remix-run/react'
+import * as styles from '../styles/_index.css'
 
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Kanban</h1>
+      <h1 className={styles.heading}>Welcome to Kanban</h1>
       <Link to="/boards">Boards</Link>
     </div>
   )
