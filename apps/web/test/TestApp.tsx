390245a0a6d8977568ea7cab06ea8601c8a7324c
AlexKMarshall <alexmarshall@qogita.com>
2023-03-02 17:55:49 +0000

    test: randomize network delay

--- apps/web/test/TestApp.tsx
+++ apps/web/test/TestApp.tsx
@@ -28,7 +28,7 @@ type TestAppStoryProps = {
   boards: Array<Parameters<typeof buildBoard>[0]>
   columns: Array<Parameters<typeof buildColumn>[0]>
   tasks: Array<Parameters<typeof buildTask>[0]>
-  connection: 'very fast' | 'fast' | 'slow' | 'very slow'
+  connection: 'instant' | 'very fast' | 'fast' | 'slow' | 'very slow'
 }
 
 export const testAppStoryDefaultProps = {
@@ -37,11 +37,15 @@ export const testAppStoryDefaultProps = {
   connection: 'very fast',
 } satisfies Partial<TestAppStoryProps>
 
-const networkDelayMap: Record<TestAppStoryProps['connection'], number> = {
-  'very fast': 0,
-  fast: 100,
-  slow: 500,
-  'very slow': 1000,
+const randomBetween = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min)
+
+const networkDelayMap: Record<TestAppStoryProps['connection'], () => number> = {
+  instant: () => 0,
+  'very fast': () => randomBetween(0, 100),
+  fast: () => randomBetween(100, 500),
+  slow: () => randomBetween(500, 1000),
+  'very slow': () => randomBetween(1000, 2000),
 }
 
 export function TestAppStory({
@@ -62,9 +66,9 @@ export function TestAppStory({
 type TestAppProps = {
   url: string
   context: TestContext
-  delay?: number
+  delay?: () => number
 }
-function TestApp({ url, context, delay = 0 }: TestAppProps) {
+function TestApp({ url, context, delay = () => 0 }: TestAppProps) {
   const middleware: Middleware = (fn) => async (args) => {
     await sleep(0)
     let response
@@ -86,7 +90,7 @@ function TestApp({ url, context, delay = 0 }: TestAppProps) {
       }
       throw response
     }
-    await sleep(delay)
+    await sleep(delay())
     return response
   }
 
