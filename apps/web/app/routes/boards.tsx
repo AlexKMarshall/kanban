ec17f45966160cc0b33659d4a0422dac96a5430f
AlexKMarshall <alexmarshall@qogita.com>
2023-03-03 08:40:04 +0000

    style: add basic grid layout

--- apps/web/app/routes/boards.tsx
+++ apps/web/app/routes/boards.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { Link, Outlet, useLoaderData } from '@remix-run/react'
+import * as styles from '../styles/boards.css'
 
 export async function loader({ context }: LoaderArgs) {
   const boards = await context.db.board.findMany()
@@ -11,21 +12,25 @@ export async function loader({ context }: LoaderArgs) {
 export default function Boards() {
   const boards = useLoaderData<typeof loader>()
   return (
-    <div>
-      <h1>Boards</h1>
-      {boards.length > 0 ? (
-        <nav>
-          <ul>
-            {boards.map((board) => (
-              <li key={board.id}>
-                <Link to={`${board.id}`}>{board.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      ) : null}
-      <Link to="new">Create New Board</Link>
-      <Outlet />
+    <div className={styles.layout}>
+      <h1 className={styles.header}>Boards</h1>
+      <div className={styles.nav}>
+        {boards.length > 0 ? (
+          <nav>
+            <ul>
+              {boards.map((board) => (
+                <li key={board.id}>
+                  <Link to={`${board.id}`}>{board.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        ) : null}
+        <Link to="new">Create New Board</Link>
+      </div>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
     </div>
   )
 }
---
+++ apps/web/app/styles/boards.css.ts
@@ -0,0 +1,23 @@
+import { style } from '@vanilla-extract/css'
+
+export const layout = style({
+  display: 'grid',
+  minHeight: '100vh',
+  gridTemplateColumns: 'auto 1fr',
+  gridTemplateRows: 'auto 1fr',
+  gridTemplateAreas: `"logo header"
+                      "nav main"`,
+  gap: '1rem',
+})
+
+export const header = style({
+  gridArea: 'header',
+})
+
+export const nav = style({
+  gridArea: 'nav',
+})
+
+export const main = style({
+  gridArea: 'main',
+})
