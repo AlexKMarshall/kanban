a357429cd8e1a8fa20bebb33bb89c956597e908f
Alex Marshall <alexmarshall@qogita.com>
2023-03-05 08:47:48 +0000

    Alexkmarshall83/ale 61 mobile navigation (#65)
    * chore: remove unused imports
    
    * chore: install radix nav menu
    
    * chore: update lockfile
    
    * feat: add mobile navigation
    
    * style mobile nav trigger

--- apps/web/app/styles/boards.css.ts
+++ apps/web/app/styles/boards.css.ts
@@ -44,12 +44,28 @@ export const boardName = style({
   fontSize: '1.125rem',
   fontWeight: 700,
   lineHeight: 1.25,
+  display: 'none',
   '@media': {
     'screen and (min-width: 768px)': {
+      display: 'block',
       fontSize: '1.25rem',
     },
     'screen and (min-width: 1024px)': {
       fontSize: '1.5rem',
+    },
+  },
+})
+
+export const mobileNavTrigger = style({
+  fontSize: '1.125rem',
+  fontWeight: 700,
+  lineHeight: 1.25,
+  background: 'none',
+  border: 'none',
+  padding: 0,
+  '@media': {
+    'screen and (min-width: 768px)': {
+      display: 'none',
     },
   },
 })
