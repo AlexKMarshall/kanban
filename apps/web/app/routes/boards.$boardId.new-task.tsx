406376b66e27282b1a8afd4449c9c0840163470a
AlexKMarshall <alexmarshall@qogita.com>
2023-03-12 11:03:06 +0000

    style: animate in the new task dialog

--- apps/web/app/routes/boards.$boardId.new-task.tsx
+++ apps/web/app/routes/boards.$boardId.new-task.tsx
@@ -9,6 +9,8 @@ import { json, redirect } from '@remix-run/server-runtime'
 import { z } from 'zod'
 import * as styles from '../styles/boards.$boardId.new-task.css'
 import * as Dialog from '@radix-ui/react-dialog'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export async function loader({ params, context }: LoaderArgs) {
   const { boardId } = params
@@ -97,66 +99,77 @@ export default function BoardsBoardIdNewTaskRoute() {
   const { columns } = useLoaderData<typeof loader>()
   const error = useActionData<typeof action>()
   const navigate = useNavigate()
+  const [open, setOpen] = useState(true)
 
   return (
     <Dialog.Root
-      open={true}
+      open={open}
       onOpenChange={(isOpen) => {
         if (!isOpen) {
           navigate(-1)
         }
       }}
     >
-      <Dialog.Overlay className={styles.dialogOverlay} />
-      <Dialog.Content className={styles.dialogContent}>
-        <Dialog.Title asChild>
-          <h2>Add new task</h2>
-        </Dialog.Title>
-        <Form method="post">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            aria-invalid={error?.fieldErrors?.title?.length ? true : undefined}
-            aria-errormessage={
-              error?.fieldErrors?.title?.length ? 'title-error' : undefined
-            }
-          />
-          {error?.fieldErrors?.title?.length ? (
-            <ul id="title-error">
-              {error.fieldErrors.title.map((error) => (
-                <li key={error}>{error}</li>
-              ))}
-            </ul>
-          ) : null}
-          <label htmlFor="column">Column</label>
-          <select
-            name="columnId"
-            id="column"
-            aria-invalid={
-              error?.fieldErrors.columnId?.length ? true : undefined
-            }
-            aria-errormessage={
-              error?.fieldErrors.columnId?.length ? 'column-error' : undefined
-            }
-          >
-            {columns.map((column) => (
-              <option key={column.id} value={column.id}>
-                {column.name}
-              </option>
-            ))}
-          </select>
-          {error?.fieldErrors?.columnId?.length ? (
-            <ul id="column-error">
-              {error.fieldErrors.columnId.map((error) => (
-                <li key={error}>{error}</li>
-              ))}
-            </ul>
-          ) : null}
-          <button type="submit">Create task</button>
-        </Form>
-      </Dialog.Content>
+      <Dialog.Portal>
+        <Dialog.Overlay className={styles.dialogOverlay} asChild>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} />
+        </Dialog.Overlay>
+        <Dialog.Content className={styles.dialogContent} asChild>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <Dialog.Title asChild>
+              <h2>Add new task</h2>
+            </Dialog.Title>
+            <Form method="post">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                aria-invalid={
+                  error?.fieldErrors?.title?.length ? true : undefined
+                }
+                aria-errormessage={
+                  error?.fieldErrors?.title?.length ? 'title-error' : undefined
+                }
+              />
+              {error?.fieldErrors?.title?.length ? (
+                <ul id="title-error">
+                  {error.fieldErrors.title.map((error) => (
+                    <li key={error}>{error}</li>
+                  ))}
+                </ul>
+              ) : null}
+              <label htmlFor="column">Column</label>
+              <select
+                name="columnId"
+                id="column"
+                aria-invalid={
+                  error?.fieldErrors.columnId?.length ? true : undefined
+                }
+                aria-errormessage={
+                  error?.fieldErrors.columnId?.length
+                    ? 'column-error'
+                    : undefined
+                }
+              >
+                {columns.map((column) => (
+                  <option key={column.id} value={column.id}>
+                    {column.name}
+                  </option>
+                ))}
+              </select>
+              {error?.fieldErrors?.columnId?.length ? (
+                <ul id="column-error">
+                  {error.fieldErrors.columnId.map((error) => (
+                    <li key={error}>{error}</li>
+                  ))}
+                </ul>
+              ) : null}
+              <button type="submit">Create task</button>
+            </Form>
+          </motion.div>
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   )
 }
